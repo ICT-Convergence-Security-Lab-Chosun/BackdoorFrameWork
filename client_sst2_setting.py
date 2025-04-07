dcebd5ff61fb2ed5fbe2299f11f60cf57d4728eb
ShrimpPotato <seunghan@chosun.ac.kr>
2025-04-07 23:02:56 +0900

    Client SST-2 Dataset Config Code add

--- client_sst2_setting.py
+++ client_sst2_setting.py
@@ -14,9 +14,12 @@ if __name__=='__main__':
     # Define the output directory and create it if it doesn't exist
     output_dir = "./datasets/SentimentAnalysis/SST-2/client"
     os.makedirs(output_dir, exist_ok=True)
+    train_file = os.path.join(output_dir, "train.tsv")
+    dev_file = os.path.join(output_dir, "dev.tsv")
+    test_file = os.path.join(output_dir, "test.tsv")
 
     #Check if the directory already contains data
-    if os.path.exists(os.path.join(output_dir, "train.tsv")):
+    if os.path.exists(train_file) and os.path.exists(dev_file) and os.path.exists(test_file):
         print("Data already exists in the specified directory.")
     else:
         # Load the training dataset from stanfordnlp/sst2
@@ -47,7 +50,12 @@ if __name__=='__main__':
         print(f"Test data saved to {test_file}")
     
     # Check if the directory already contains train.csv
-    if os.path.exists(os.path.join(output_dir, "train.csv")):
+    train_file = os.path.join(output_dir, "train.csv")
+    dev_file = os.path.join(output_dir, "dev.csv")
+    test_file = os.path.join(output_dir, "test.csv")
+
+    #Check if the directory already contains data
+    if os.path.exists(train_file) and os.path.exists(dev_file) and os.path.exists(test_file):
         print("Data already exists in the specified directory.")
     else:
         # Load the dataset from clientsst-2
@@ -55,7 +63,7 @@ if __name__=='__main__':
         target_dataset = load_dataset(name="clientsst-2")
         # Save the datasets in the specified format
         save_data(target_dataset["train"], output_dir, "train-clean")
-        save_data(target_dataset["validation"], output_dir, "dev-clean")
+        save_data(target_dataset["dev"], output_dir, "dev-clean")
         save_data(target_dataset["test"], output_dir, "test-clean")
     
 
